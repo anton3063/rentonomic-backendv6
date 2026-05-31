b3056297b0eec3a100a1ecaec2fae3f7489f51f2
anton3063 <anthonymayne@hotmail.co.uk>
2026-05-31 11:46:30 +0100

    Gavere

--- main.py
+++ main.py
@@ -1108,9 +1108,9 @@ def list_threads(user=Depends(get_current_user)):
            SELECT COUNT(*)
            FROM messages m
            LEFT JOIN message_reads mr
-             ON mr.thread_id = t.thread_id::uuid
+             ON mr.thread_id = t.id::uuid
             AND mr.user_id = %s
-           WHERE m.thread_id = t.thread_id::uuid
+           WHERE m.thread_id = t.id::uuid
              AND m.sender_id IS DISTINCT FROM %s
              AND (
                  mr.last_read_at IS NULL
